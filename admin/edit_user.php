1827f276c357af877106e59e7027e5e183ee422d
Adna Jasarevic <adnaja011@gmail.com>
2024-05-21 22:52:04 +0200

    .

---
+++ admin/edit_user.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+require '../includes/db_connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $sql = "UPDATE people SET username='{$_POST['username']}', email='{$_POST['email']}', f_name='{$_POST['f_name']}', 
+                    l_name='{$_POST['l_name']}', DOB='{$_POST['DOB']}', phone='{$_POST['phone']}' WHERE PID='{$_POST['PID']}'";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Error updating user: " . $conn->error;
+    }
+}
+
+$user = [];
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM people WHERE PID='$id'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+    } else {
+        echo "User not found.";
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit User</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Edit Brand</h1>
+<form action="" method="post">
+    <label for="PID">User ID:</label>
+    <input type="text" id="PID" name="PID" value="<?php echo $user['PID'] ?>" readonly><br>
+
+    <label for="f_name">New First Name:</label>
+    <input type="text" id="f_name" name="f_name" value="<?php echo $user['f_name'] ?>"><br>
+
+    <label for="l_name">New Last Name:</label>
+    <input type="text" id="l_name" name="l_name" value="<?php echo $user['l_name'] ?>"><br>
+
+    <label for="username">New Username:</label>
+    <input type="text" id="username" name="username" value="<?php echo $user['username'] ?>"><br>
+
+    <label for="pass">New Password:</label>
+    <input type="text" id="pass" name="pass" value="<?php echo $user['p_password'] ?>"><br>
+
+    <label for="dob">New Date of Birth:</label>
+    <input type="text" id="dob" name="dob" placeholder="YYYY-MM-DD" value="<?php echo $user['DOB'] ?>"><br>
+
+    <label for="phone">New Phone:</label>
+    <input type="text" id="phone" name="phone" value="<?php echo $user['phone'] ?>"><br>
+
+    <label for="email">New Email:</label>
+    <input type="text" id="email" name="email" value="<?php echo $user['email'] ?>"><br>
+
+    <button type="submit">Edit User</button>
+</form>
+</body>
+</html>
--- home_page.php
+++ home_page.php
@@ -1,5 +1,4 @@
-<?php require 'includes/header_sessions.php'?>
-
+<?php require 'includes/header_sessions.php'; ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,6 +10,8 @@
     <link rel="stylesheet" href="includes/header.css">
     <link rel="stylesheet" href="home-page.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 
 <body class="home">
@@ -19,6 +20,46 @@
     <input id="search-input" type="text" placeholder="Search...">
     <div id="search-results"></div>
 </div>
+
+<div class="header-image">
+    <h1>PORODICA KARALIĆ</h1>
+</div>
+<h2 class="popular-picks">Most popular picks</h2>
+<div class="container">
+    <div class="slider">
+        <button id="prev-slide" class="slide-button material-symbols-rounded">
+            chevron_left
+        </button>
+        <ul class="image-list">
+
+            <?php
+            require 'includes/db_connection.php';
+
+            $sql = "SELECT pid, p_image FROM products LIMIT 7";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<a href="product.php?pid=' . $row["pid"] . '"><img class="image-item" src="' . $row["p_image"] . '" alt="Product Image" /></a>';
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+            ?>
+
+        </ul>
+        <button id="next-slide" class="slide-button material-symbols-rounded">
+            chevron_right
+        </button>
+    </div>
+    <div class="slider-scrollbar">
+        <div class="scrollbar-track">
+            <div class="scrollbar-thumb"></div>
+        </div>
+    </div>
+</div>
 </body>
 <script src="includes/header.js"></script>
+<script src="slider.js" defer></script>
 </html>
