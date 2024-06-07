c827bbfcec7e4ce1f38225a16fac0734eb022dde
karalicemina <148528194+karalicemina@users.noreply.github.com>
2024-06-07 20:36:46 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<footer>
+    <div class="footer-container">
+        <div class="footer">
+            <div class="footer-heading footer-1">
+                <h2>Socials</h2>
+                <a href="https://www.instagram.com/zlatara.porodicakaralic/" class="fa fa-instagram"> Instagram</a>
+
+                <a href="https://www.facebook.com/p/Zlatara-Porodica-Karali%C4%87Visoko-100057578735152/" class="fa fa-facebook"> Facebook</a>
+            </div>
+
+            <div class="footer-heading footer-2">
+                <h2> Working hours </h2>
+                <p> Monday - Saturday </p>
+                <p class="fa fa-clock-o"> 08:30h - 18:00h  </p>
+            </div>
+
+            <div class="footer-heading footer-3">
+                <h2>Contact information</h2>
+                <p class="fa fa-phone"> +387 61 780 374</p>
+                <p class="fa fa-envelope"> zlataraporodicakaralic@gmail.com </p>
+            </div>
+
+            <div class="footer-heading footer-4">
+                <h2> Location </h2>
+                <p class="fa fa-location-arrow"> Čaršijska 55, Visoko </p>
+                <p class="fa fa-location-arrow"> Šareni Hanovi 33, Visoko </p>
+            </div>
+        </div>
+
+        <div class="footerBottom">
+            <p>Copyright &copy;2024 </p>
+        </div>
+    </div>
+</footer>
+
+
